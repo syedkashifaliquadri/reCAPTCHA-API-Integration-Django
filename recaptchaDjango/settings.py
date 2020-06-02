adc0e9e96284d4718505a679d6899dd87cdb7a6c
Syed Kashif Ali <mehmoodkashif09@gmail.com>
2020-06-03 02:18:18 +0500

    Update settings.py

--- recaptchaDjango/settings.py
+++ recaptchaDjango/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if os.getcwd() == '/app':
     import dj_database_url
